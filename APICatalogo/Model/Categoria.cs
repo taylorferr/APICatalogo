ab6d128a32d855d4eb4ae1eb124295e03a8fa0ba
taylor.pereira <taylor.pereira@southsystem.com.br>
2022-09-25 16:21:53 -0300

    Adicionar arquivos de projeto.

---
+++ APICatalogo/Model/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+
+namespace APICatalogo.Model;
+
+public class Categoria
+{
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+    public ICollection<Produto>? Produtos { get; set; }
+}
